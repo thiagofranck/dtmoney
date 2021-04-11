f68d1fa586bb865fdf6db3882becf701749541ff
Thiago Henrique <thiagohenrique776@gmail.com>
2021-04-11 16:08:06 -0300

    Estrutura do form e estilização do modal

--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -1,4 +1,6 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
+import closeImg from '../../assets/close.svg'
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -10,8 +12,34 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
     <Modal
         isOpen={isOpen} 
         onRequestClose={onRequestClose}
-    >          
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content"
+    >   
+        <button type="button" onClick={onRequestClose} className="react-modal-close">
+            <img src={closeImg} alt="Fechar Modal"/>
+        </button>
+        
+        <Container>  
         <h2>Cadastrar Transação</h2>
+
+        <input 
+            placeholder="Titulo"
+        />
+
+        <input 
+            type="number"
+            placeholder="Valor"
+        />  
+
+        <input 
+            placeholder="Titulo"
+        />
+
+        <button type="submit">
+            Cadastrar
+        </button>
+        </Container>
+        
     </Modal>
     );
 }
